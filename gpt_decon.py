fbecd30b0802e125c44f70fc9cd5b63082cacdd6
Chen Yu <chenyu.nus@gmail.com>
2023-02-28 00:08:16 +0800

    more

--- gpt_decon.py
+++ gpt_decon.py
@@ -27,7 +27,7 @@ class LN(Pytree):
         self.b = b
 
     # x: (n_channels,)
-    def __call__(self, x: Arr) -> Arr:
+    def f(self, x: Arr) -> Arr:
         o = x - mean(x)
         i = self.w * rsqrt(var(x) + self.eps)
         return o * i + self.b
@@ -38,7 +38,8 @@ class Linear(Pytree):
         self.w = w
         self.b = b
 
-    def __call__(self, x: Arr) -> Arr:
+    # x: (n_channels,)
+    def f(self, x: Arr) -> Arr:
         return self.w @ x + self.b
 
 
@@ -48,6 +49,7 @@ class GptMha(Pytree):
     dim_head: int = static_field()
     T: int = static_field()
     mask: Arr = static_field()
+    linearf: Callable[[Arr], Arr] = static_field()
 
     # q, k, v: (dim_head, T)
     def causal_dot_attention(self, q: Arr, k: Arr, v: Arr) -> Arr:
@@ -60,15 +62,16 @@ class GptMha(Pytree):
         self.QKV = QKV
         self.linear = linear
         self.mask = jnp.tril(jnp.ones((T, T)))
+        self.linearf = xmap(linear.f, [None, 'T'], 'T')
 
     # x: (n_channels, T)
-    def __call__(self, x: Arr) -> Arr:
+    def f(self, x: Arr) -> Arr:
         q, k, v = self.QKV @ x
         xmap_in = [['n_head', ...]] * 3
         attended = xmap(self.causal_dot_attention, xmap_in, 'n_head')(q, k, v)
         # attended: (n_heads, dim_head, T)
-        jnp.concatenate(attended)
-        return self.linear(attended)
+
+        return self.linearf(jnp.concatenate(attended))
 
 
 class GptFfn(Pytree):
@@ -76,17 +79,38 @@ class GptFfn(Pytree):
         self.linear1 = linear1
         self.linear2 = linear2
 
-    def __call__(self, x: Arr) -> Arr:
-        return self.linear2(gelu(self.linear1(x)))
+    # x: (n_channels,)
+    def f(self, x: Arr) -> Arr:
+        return self.linear2.f(gelu(self.linear1.f(x.T))).T
 
 
 class GptBlock(Pytree):
+    ln1f: Callable[[Arr], Arr] = static_field()
+    ln2f: Callable[[Arr], Arr] = static_field()
+    ffnf: Callable[[Arr], Arr] = static_field()
+
     def __init__(self, mha: GptMha, ffn: GptFfn, ln1: LN, ln2: LN):
         self.mha = mha
         self.ffn = ffn
         self.ln1 = ln1
         self.ln2 = ln2
+        self.ln1f = xmap(ln1.f, [None, 'T'], 'T')
+        self.ln2f = xmap(ln2.f, [None, 'T'], 'T')
+        self.ffnf = xmap(ffn.f, [None, 'T'], 'T')
 
     # x: (n_channels, T)
     def __call__(self, x: Arr) -> Arr:
-        x += self.mha(self.ln1(x))
+        x += self.mha.f(self.ln1f(x))
+        x += self.ffnf(self.ln2f(x))
+        return x
+
+
+class GptDecoder(Pytree):
+    def __init__(self, blocks: list[GptBlock]):
+        self.blocks = blocks
+
+    # x: (n_channels, T)
+    def __call__(self, x: Arr) -> Arr:
+        for block in self.blocks:
+            x = block(x)
+        return x
